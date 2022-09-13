cefe1417e4ba02d8a2c1c2b5197c482dc54e1cd2
xJaimeLx <jaimelemu@hotmail.com>
2022-09-12 21:08:20 -0500

    Subiendo actualizacion

--- server/routes/Producto.js
+++ server/routes/Producto.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
 
-router.post('/', productoController.crearProducto);
-router.get('/', productoController.obtenerProductos);
-router.put('/:id', productoController.actualizarProducto);
-router.get('/:id', productoController.obtenerProducto);
-router.delete('/:id', productoController.eliminarProducto);
+router.post('/', productoController.crearProducto); //Agregar o crear
+router.get('/', productoController.obtenerProductos); //Mostrar
+router.put('/:id', productoController.actualizarProducto); // Actualizar
+router.get('/:id', productoController.obtenerProducto); // obtener 
+router.delete('/:id', productoController.eliminarProducto); //eliminar
 
 module.exports = router;
